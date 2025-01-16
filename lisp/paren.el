6f3e0f1b70b30746cdbbfbb2444575330a5fa96c
etcqez <etcqez@outlook.com>
2025-01-16 12:17:38 +0800

    更新配置文件

--- lisp/paren.el
+++ lisp/paren.el
@@ -1,10 +1,12 @@
 (use-package paren
   :init
-  (show-paren-mode +1))
+  (show-paren-mode +1)
+  )
 
 (use-package elec-pair
   :config
-  (electric-pair-mode +1))
+  (electric-pair-mode +1)
+  )
 
 (use-package rainbow-delimiters
   :ensure t
